d6d336a0e25d1f7460825489c121a911207c05b3
SuraMahesh <surangamunasinghe22568@gmail.com>
2020-09-26 14:54:09 +0530

    create reports directory & Generate report class

--- amazon_config.py
+++ amazon_config.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-DIRETORY = 'reports'
+DIRECTORY = 'reports'
 NAME = 'PS4'
 CURRENCY = '$'
 MIN_PRICE = '275'
@@ -17,10 +17,12 @@ def get_chrome_web_driver(options):
 def get_web_driver_options():
     return webdriver.ChromeOptions()
 
-def set_ignore_cetificate_error(options):
+def set_ignore_certificate_error(options):
     options.add_argument('--ignore-certificate-errors')
 
 def set_browser_as_incognito(options):
     options.add_argument('--incognito')      
 
+def set_automation_as_head_less(options):
+    options.add_argument('--headless')
 
